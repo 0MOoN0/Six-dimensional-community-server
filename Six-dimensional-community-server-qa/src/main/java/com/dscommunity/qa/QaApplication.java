12425b2ad882053f357b68b79798f2226420abbb
0MOoN0 <814429032@qq.com>
2020-01-03 15:10:10 +0800

    [UPDATE/已测试]配置QA模块的Eureka客户端并注册到Eureka服务器

--- Six-dimensional-community-server-qa/src/main/java/com/dscommunity/qa/QaApplication.java
+++ Six-dimensional-community-server-qa/src/main/java/com/dscommunity/qa/QaApplication.java
@@ -2,11 +2,13 @@ package com.dscommunity.qa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import utils.IdWorker;
 import utils.JwtUtil;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class QaApplication {
     public static void main(String[] args) {
         SpringApplication.run(QaApplication.class, args);
