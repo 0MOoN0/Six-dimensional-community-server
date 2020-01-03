b3393e1c5c8166de94a15325c8bee558a9166a38
0MOoN0 <814429032@qq.com>
2020-01-03 14:37:50 +0800

    [UPDATE/已测试]完成Article的Eureka客户端配置、注册到服务端

--- Six-dimensional-community-server-article/src/main/java/com/sdcommunity/article/ArticleApplication.java
+++ Six-dimensional-community-server-article/src/main/java/com/sdcommunity/article/ArticleApplication.java
@@ -1,10 +1,12 @@
 package com.sdcommunity.article;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import utils.IdWorker;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ArticleApplication {
 
 	public static void main(String[] args) {
