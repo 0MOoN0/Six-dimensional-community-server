53ab6bf534cb570704e8a0b35fdac6fe419e9e59
0MOoN0 <814429032@qq.com>
2020-01-04 12:58:35 +0800

    [UPDATE/已测试]Manager模块 修改后台网关Zuul过滤器，放行登陆URL，对其他URL拦截并作权限鉴定

--- Six-dimensional-community-server-manager/src/main/java/com/sdcommunity/manager/ManagerApplication.java
+++ Six-dimensional-community-server-manager/src/main/java/com/sdcommunity/manager/ManagerApplication.java
@@ -3,6 +3,8 @@ package com.sdcommunity.manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import utils.JwtUtil;
 
 /**
  * 后台网关
@@ -13,5 +15,11 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class ManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManagerApplication.class,args);
+    }
+
+
+    @Bean
+    public JwtUtil jwtUtil(){
+        return new JwtUtil();
     }
 }
